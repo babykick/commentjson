66282ec934847dfc88ef8d84856412a99bb09630
babykick <babykick@163.com>
2017-03-31 16:53:08 +0800

    fix import error

--- commentjson/__init__.py
+++ commentjson/__init__.py
@@ -1,5 +1,1 @@
-from commentjson import dump
-from commentjson import dumps
-from commentjson import JSONLibraryException
-from commentjson import load
-from commentjson import loads
+from .commentjson import (dump, dumps, JSONLibraryException, load, loads)
--- commentjson/commentjson.py
+++ commentjson/commentjson.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Add JavaScript or Python style comments in JSON.
 
